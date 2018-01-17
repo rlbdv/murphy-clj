a3577446c031fde4a3ef538f5bd6bcca6f60a24a
Rob Browning <rlb@defaultvalue.org>
2018-01-17 00:38:36 -0600

    Update project.clj for 0.3.0 development
    Signed-off-by: Rob Browning <rlb@defaultvalue.org>

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject murphy "0.2.0"
+(defproject murphy "0.3.0-SNAPSHOT"
   :description "Clojure library for better handling of bad situations."
   :url "https://gitlab.com/clj-murphy/murphy"
   :licenses [{:name "GNU Lesser General Public License, version 2.1 or newer"
