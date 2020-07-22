d75bcb3a53114d1c687b78fb7b577d43683d2d47
Rob Browning <rlb@defaultvalue.org>
2020-07-21 19:31:11 -0500

    project: update to clojure 1.10.1
    Signed-off-by: Rob Browning <rlb@defaultvalue.org>

--- project.clj
+++ project.clj
@@ -1,8 +1,8 @@
-(defproject murphy "0.5.0"
+(defproject murphy "0.5.1"
   :description "Clojure library for better handling of bad situations."
   :url "https://gitlab.com/clj-murphy/murphy"
   :licenses [{:name "GNU Lesser General Public License, version 2.1 or newer"
               :url "https://www.gnu.org/licenses/lgpl-2.1.html"}
              {:name "Eclipse Public License 1.0 or newer"
               :url "http://www.eclipse.org/legal/epl-v10.html"}]
-  :dependencies [[org.clojure/clojure "1.10.0"]])
+  :dependencies [[org.clojure/clojure "1.10.1"]])
