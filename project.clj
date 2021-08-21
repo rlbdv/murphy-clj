b341414327f2bc37e66732760c50655139d4f638
Rob Browning <rlb@defaultvalue.org>
2021-08-21 14:47:10 -0500

    Update to clojure 1.10.2
    Signed-off-by: Rob Browning <rlb@defaultvalue.org>

--- project.clj
+++ project.clj
@@ -5,7 +5,7 @@
               :url "https://www.gnu.org/licenses/lgpl-2.1.html"}
              {:name "Eclipse Public License 1.0 or newer"
               :url "http://www.eclipse.org/legal/epl-v10.html"}]
-  :dependencies [[org.clojure/clojure "1.10.1"]]
+  :dependencies [[org.clojure/clojure "1.10.2"]]
   :profiles {:eastwood {:plugins [[jonase/eastwood "0.9.7"]]}}
   :eastwood {:config-files ["eastwood.clj"]}
   :aliases {"eastwood" ["with-profile" "+eastwood" "eastwood"]
